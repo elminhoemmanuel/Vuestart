fa9e1e0fbda0755a9fe4d29970e1bb4695ffeb9e
elminhoemmanuel <amodue15@gmail.com>
2021-09-30 11:14:23 -0700

    working

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,118 @@
 <template>
-  <!-- <h2>Hello</h2>
-  <h2>{{ add( 2, 4 , 6) }}</h2>
-  <h2>{{ multiply( 6 ) }}</h2> -->
-  <!-- <p>{{name}}</p>
-  <button v-on:mouseout="name = 'Abobo'">click</button> -->
-
-  <!-- <div>
-    <h3>{{count}}</h3>
-    <button v-on:click="increase(5)">Increase</button>
-    <button v-on:click="decrease(5)">Decrease</button>
-  </div> -->
   <div>
-    <h3>{{count}}</h3>
-    <button @click="increase(5, $event)">Increase</button>
-    <button @click="decrease(5)">Decrease</button>
+    <pre>
+      {{JSON.stringify(formValues, null, 2)}}
+    </pre>
   </div>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model.trim.lazy="formValues.name">
+    </div>
+
+    <div>
+      <label for="profile">Bio</label>
+      <textarea type="text" id="profile" v-model="formValues.profileSummary"></textarea>
+    </div>
+
+    <div>
+      <label for="country">Country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+
+    <div>
+      <label for="job-location">Job Location</label>
+      <select id="job-location" multiple v-model="formValues.jobLocation">
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+
+    <div>
+      <input 
+      type="checkbox" 
+      name="remoteWork" 
+      id="remoteWork" 
+      v-model="formValues.remoteWork"
+      true-value="yes"
+      false-value="no"
+      >
+      <label for="remoteWork">Open to working remotely?</label>
+    </div>
+
+    <div>
+      <label>Skill set</label>
+      <input
+        type="checkbox"
+        id="html"
+        value="html"
+        v-model="formValues.skillSet"
+      />
+      <label for="html">HTML</label>
+      <input
+        type="checkbox"
+        id="css"
+        value="css"
+        v-model="formValues.skillSet"
+      />
+      <label for="css">CSS</label>
+      <input
+        type="checkbox"
+        id="javascript"
+        value="javascript"
+        v-model="formValues.skillSet"
+      />
+      <label for="javascript">JavaScript</label>
+    </div>
+
+    <div>
+      <label>Years of Experience</label>
+      <input
+        type="radio"
+        id="0-2"
+        value="0-2"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="0-2">0-2</label>
+      <input
+        type="radio"
+        id="3-5"
+        value="3-5"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="3-5">3-5</label>
+      <input
+        type="radio"
+        id="6-10"
+        value="6-10"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="6-10">5-10</label>
+      <input
+        type="radio"
+        id="10+"
+        value="10+"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="10+">10+</label>
+    </div>
+
+    <div>
+      <label for="age">Age</label>
+      <input type="number" id="age" v-model.number="formValues.age" />
+    </div>
+
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+
+  </form>
   
   
 </template>
@@ -24,25 +122,22 @@ export default {
   name: "App",
   data() {
     return {
-      multiplier: 10,
-      name:'omale',
-      count: 0,
+      formValues: {
+        name:"",
+        profileSummary:"",
+        country:"",
+        jobLocation:"",
+        remoteWork: "no",
+        skillSet:[],
+        yearsOfExperience:[],
+        age:null
+      }
     };
   },
   methods : {
-    // add (a, b, c){
-    //   return a + b + c
-    // },
-    // multiply ( num ){
-    //   return num * this.multiplier
-    // }
-    increase(num, event){
-      this.count += num,
-      console.log("event was", event)
-    },
-    decrease(num){
-      this.count -= num
-    },
+    submitForm(){
+      console.log("form values are", this.formValues)
+    }
   }
 };
 </script>
@@ -52,8 +147,34 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 20px;
+  
+}
+
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+input[type='text'],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 </style>
